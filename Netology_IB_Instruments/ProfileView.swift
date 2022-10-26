9434843789e3c1c1acc77ba7ea73352d0bdeeb25
fedorgladkov <fedor.9619@gmail.com>
2022-10-26 10:56:44 +0700

    IOSUI-CP-1_HW_1 Homework was done

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Федор Гладков on 20.10.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UITextView!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
